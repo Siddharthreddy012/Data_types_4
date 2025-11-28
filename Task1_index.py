5c36fe2890239d10c258d6b2aa776a94df125c4b
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-11-28 21:58:43 +0530

    indexing

---
+++ Task1_index.py
@@ -0,0 +1,14 @@
+word = "AMUDALA VEKATA SIDDHARTH REDDY"
+print(word)
+# 3rd occurrence of space in +ve direction
+print(word[24])
+# 5th occurrence of A in +ve direction
+print(word[13])
+# 5th occurrence of A in -ve direction
+print(word[-17])
+# 1st occurrence of D in -ve direction
+print(word[-27])
+# Print median space in your name
+print(word[14])
+# 6th occurrence of A in +ve direction
+print(word[20]) 
---
+++ Task2_index.py
@@ -0,0 +1,11 @@
+word = "EAGLE WOOD  "
+print(word)
+# G in +ve direction
+print(word[2]) 
+# 1st occarance of O in -ve direction
+print(word[-4])
+# Third occarace of space in +ve direction
+print(word[11])
+# Third occarace of space in -ve direction
+print(word[-7])
+# print small d using indexing. 
